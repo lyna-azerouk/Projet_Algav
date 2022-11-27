a47e33ef5e304c76117d71822b58eba92b4cdc3c
Lina.Azerouk <lina.azerouk@etu.sorbonne-universite.fr>
2022-11-27 17:33:34 +0100

    qst 3.10 non terminé

--- partie2.py
+++ partie2.py
@@ -6,6 +6,7 @@ class Arbre():
         self.mot_luka=''
         self.G = None
         self.D = None
+        self.p=None 
 
 def cons_arbre   ( T):
     liste =decomposition (len (T))
@@ -40,22 +41,78 @@ resultat = table (38,8)
 
 def affiche(arbre ):
     if  arbre !=None :
-        return  (  print (arbre.label), affiche( arbre.G)  ,affiche( arbre.D) )
+        return  (  print (arbre.mot_luka), affiche( arbre.G)  ,affiche( arbre.D) )
 #print("parcours prefefixe de l'arbre ")      
 arbre_decision=(cons_arbre(resultat)) 
-
+#affiche(arbre_decision)
 def luka ( arbre ) : 
     if arbre ==None  :  return  arbre
     else :
         if arbre.G==None and arbre.D==None :
-                 arbre.mot_luka=arbre.label       
+                 arbre.mot_luka= arbre.label     
                  return arbre 
         else :
-           arbre.mot_luka= '(' + arbre.label + arbre.mot_luka+ str( (luka(arbre.G)).mot_luka )+ str( luka(arbre.D).mot_luka ) + ')'
+           arbre.mot_luka= arbre.label + '('+ arbre.mot_luka+ str( (luka(arbre.G)).mot_luka ) + ')'+ '('+ str( luka(arbre.D).mot_luka )  +')'
            return arbre
 
 
 arbre_luka = luka(arbre_decision)
-affiche(arbre_luka)
+#affiche(arbre_luka) 
+
+#qst 3.10
+def compression_2 ( arbre , liste ):
+   if arbre==None : return  arbre 
+   else :
+    #1ere regle
+    if  arbre.G!=None and arbre.G.mot_luka ==False  :
+        arbre.G=liste[1]  ;   compression_2(arbre.D,liste) ;   compression_2(arbre.G,liste)
+    if arbre.D!=None and arbre.D.mot_luka==False :
+        arbre.D=liste[1];   compression_2(arbre.G,liste)  ;    compression_2(arbre.D,liste) 
+    if arbre.G!=None and arbre.G.mot_luka ==True  :
+        arbre.G=liste[0] ;   compression_2(arbre.D,liste)  ;compression_2(arbre.G,liste)
+    if arbre.D!=None and arbre.D.mot_luka ==True  :
+        arbre.D=liste[0]  ; compression_2(arbre.G,liste);  compression_2(arbre.D,liste) 
+    #2eme regle
+    if arbre.G!=None and arbre.G.G!=None and arbre.G.D!=None   and   arbre.G.G.mot_luka==arbre.G.D.mot_luka  :
+        arbre.G=arbre.G.G ;  compression_2 ( arbre.D,liste)  ;  compression_2 ( arbre.G,liste) 
+    if arbre.D!=None and arbre.D.G!=None and arbre.D.D!=None   and   arbre.D.G.mot_luka==arbre.D.D.mot_luka  :
+        arbre.D=arbre.D.G  ; compression_2( arbre.G ,liste) ; compression_2 ( arbre.D,liste) 
+    else :
+        compression_2 ( arbre.G, liste )  ; compression_2 ( arbre.D, liste )
+    return arbre 
+    
+
+def  compression_bdd ( arbre  ):
+    if arbre ==None : return  arbre 
+    noeudf= Arbre (False )
+    noeudt=Arbre(True)
+    noeudf.mot_luka=False 
+    noeudt.mot_luka=True 
+    noeudf.label=False 
+    noeudt.label=True 
+    noeudf.D=None
+    noeudf.G=None 
+    noeudt.G=None 
+    noeudt.D=None
+    l=[]
+    l.append(noeudt)
+    l.append(noeudf)
+
+    resultat=compression_2(arbre ,l)
+    return resultat
+
+arbre_Robdd= compression_bdd(arbre_luka)
+affiche(arbre_Robdd) 
+
+
+
+
+
+    
+
+         
+
+
+
 
 
