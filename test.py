d4c5cffd51f992918d7f8d5fabb93562c9906f6f
fmran <frmarcelon@gmail.com>
2022-12-15 08:18:47 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,50 +1,21 @@
-#creation de l'arbre exemple (figure 1 de l'enonce)
+from partie1 import *
+from p2ori import *
 
-trueGGD=Arbre('TrueGGD')
-trueGGD.mot_luka="True"
-falseGGG=Arbre('FalseGGG')
-falseGGG.mot_luka="False"
-x1GG=Arbre('x1GG')
-x1GG.mot_luka="x1(False)(True)"
-x1GG.G=falseGGG
-x1GG.D=trueGGD
+#test partie 1
+table0=table(38,8)
+print(table0)
+arbre0=cons_arbre(table0)
 
-trueGDG=Arbre('TrueGDG')
-trueGDG.mot_luka="True"
-falseGDD=Arbre('FalseGDD')
-falseGDD.mot_luka="False"
-x1GD=Arbre('x1GD')
-x1GD.mot_luka="x1(True)(False)"
-x1GD.G=trueGDG
-x1GD.D=falseGDD
+#attention affichage non correcte car meme nom
+digraph_in_dot(arbre0)
 
-trueDGD=Arbre('TrueDGD')
-trueDGD.mot_luka="True"
-falseDGG=Arbre('FalseDGG')
-falseDGG.mot_luka="False"
-x1DG=Arbre('x1DG')
-x1DG.mot_luka="x1(False)(True)"
-x1DG.G=falseDGG
-x1DG.D=trueDGD
 
-falseDDG=Arbre('FalseDDG')
-falseDDG.mot_luka="False"
-falseDDD=Arbre('FalseDDD')
-falseDDD.mot_luka="False"
-x1DD=Arbre('x1DD')
-x1DD.mot_luka="x1(False)(False)"
-x1DD.G=falseDDG
-x1DD.D=falseDDD
 
-x2G=Arbre('x2G')
-x2G.mot_luka="x2(x1(False)(True))(x1(True)(False))"
-x2G.G=x1GG
-x2G.D=x1GD
-x2D=Arbre('x2D')
-x2D.mot_luka="x2(x1(False)(True))(x1(False)(False))"
-x2D.G=x1DG
-x2D.D=x1DD
-root=Arbre('rootx3')
-root.mot_luka="x3(x2(x1(False)(True))(x1(True)(False)))(x2(x1(False)(True))(x1(False)(False)))"
-root.G=x2G
-root.D=x2D
+#creation d une table
+table1=[True,True,True,False,True,True,True,False]
+
+#construction de l arbre
+arbre1=cons_arbre(table1)
+luka(arbre1)
+#sauvegarder en dot
+digraph_in_dot(arbre1)
